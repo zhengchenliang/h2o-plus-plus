d66484202ee75b24818f345d1586ebbdc70c9423
zhliang <talie.firzen@foxmail.com>
2025-11-22 04:43:48 +0800

    Add header file for A200HTTP test

---
+++ test.h
@@ -0,0 +1,14 @@
+#ifndef A200HTTPTEST1V1_HH
+#define A200HTTPTEST1V1_HH
+
+#include "h2o_plus_plus.h"
+
+#include <cassert>
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <chrono>
+#include <thread>
+#include <string>
+
+#endif
